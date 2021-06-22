4c88c63462f89d70429b2f08fe339e793f21d287
Gautier <gautier.blandin@gmail.com>
2021-06-22 16:49:26 +0200

    Add Masks and Clears for rank, files and squares

--- src/ia/chessBitBoard.py
+++ src/ia/chessBitBoard.py
@@ -11,3 +11,23 @@ class Bitboard:
     br: int
     bq: int
     bk: int
+
+
+class Move:
+    start: int
+    end: int
+    flag: int
+    promotedPiece: int
+
+
+def str_bit_board(bits: int) -> str:
+    string = ''
+    for i in range(8):
+        string += '|'
+        for j in range(8):
+            bit = bits & 0b1
+            string += str(bit)
+            string += '|'
+            bits = bits >> 1
+        string += '\n'
+    return string
