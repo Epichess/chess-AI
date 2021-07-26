4d582f6a98ce2265326e9fac9876c515f47a526e
Gautier <gautier.blandin@gmail.com>
2021-07-26 15:11:20 +0200

    Add basic AI (implementing alpha-beta pruning minimax search)

--- src/ia/search.py
+++ src/ia/search.py
@@ -1,6 +1,8 @@
 from chessBitBoard import Bitboard
 import CONSTANTS
 from bit_utils import extract_index
+from move import Move
+from queue import PriorityQueue
 
 
 def evaluate(bitboard: Bitboard) -> int:
@@ -20,3 +22,36 @@ def evaluate(bitboard: Bitboard) -> int:
     black_score += 1000 * len(extract_index(bitboard.pieces['k']))
     return white_score - black_score
 
+
+def search(board: Bitboard, current_depth: int, max_depth: int, alpha: float = -float('inf'), beta: float = float('inf')) -> tuple[float, Move or None]:
+
+    evaluation = evaluate(board)
+    # print(evaluation)
+    # print(board)
+    if abs(evaluation) > 500:
+        return evaluation, None
+    if current_depth == max_depth:
+        return evaluation, None
+
+    evals = PriorityQueue()
+    for move in board.moveGenerator.gen_moves(board):
+        # Making the move and not doing anything if it turned out to be illegal
+        if not board.make_move(move):
+            continue
+        best_child_move = search(board, current_depth + 1, max_depth)
+        board.unmake_move()
+        if board.board_info.us:
+            alpha = max(alpha, best_child_move[0])
+            if best_child_move[0] >= beta:
+                return best_child_move[0], move
+            best_child_move = (-best_child_move[0], move)
+        else:
+            beta = max(beta, best_child_move[0])
+            if best_child_move[0] <= alpha:
+                return best_child_move[0], move
+            best_child_move = (best_child_move[0], move)
+        evals.put(best_child_move)
+    best_move = evals.get()
+    if board.board_info.us:
+        best_move = (-best_move[0], best_move[1])
+    return best_move
